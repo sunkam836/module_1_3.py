a731b664dfa5cc996b24edf988b587d2c1046de0
sunkam836 <sunkam836@gmail.com>
2024-11-17 21:37:16 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+name = 'Vadim'
+print(name)
+age = 57
+print(age)
+new_age = 58
+print(new_age)
+Is_student = 'True'
+print(Is_student)
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
